462f38397371ee00345b6370a1066da47e47adce
Michael Schiumo <mschiumo18@gmail.com>
2019-05-20 17:22:23 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,13 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(name: params[:user][:name])
     user = user.try(:authenticate, params[:user][:password])
-    
+
     session[:user_id] = @user.id
   end
 
   def destroy
     session.delete :name
+    redirect_to '/'
   end
 
   def new
