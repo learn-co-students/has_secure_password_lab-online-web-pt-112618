7dc00e9ab9b1232896aec129dce83757a21fa5bd
Michael Schiumo <mschiumo18@gmail.com>
2019-05-20 17:05:20 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,4 +5,6 @@ class SessionsController < ApplicationController
     return head(:forbidden) unless @user.authenticate(params[:password])
     session[:user_id] = @user.id
   end
+
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
+    return redirect_to controller: 'users', action: 'new' unless @user.save
+    session[:user_id] = @user.id
+    redirect_to controller: 'welcome', action: 'home'
   end
 
   private
