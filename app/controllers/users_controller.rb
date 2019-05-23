928181e17e5b10a5e16d67703dcc939a9a04a881
Whitny Edwards <wne4e2@gmail.com>
2019-05-22 21:21:07 -0400

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    if params[:user][:password] != params[:user][:password_confirmation]
+      redirect_to users_new_path
+    else
+      @user = User.create(user_params)
+      session[:user_id] = @user.id
+      redirect_to welcome_path
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :password, :password_confirmation)
+    end
+
+end
